bc1f30baefa74d7e93c57e46ad54591fd3710685
AmEl <amirelkanov0504@gmail.com>
2020-07-01 23:27:47 +0300

    max_num_of_categories_per_page updated

--- extensions/const.py
+++ extensions/const.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URI = "postgres://wgfebwadpolfvi:61a91a8bd3230cc9ab9a020" \
                           "ba3517faeb998eedbe877237c112d29114ff8736f@ec2-52-" \
                           "71-55-81.compute-1.amazonaws.com:5432/d2j2hkmdp0uifl"
 
-max_num_of_categories_per_page = 2
+max_num_of_categories_per_page = 4
 
 # URL of the page the user will be redirected to
 article_url = "https://amel-tourism.herokuapp.com/article"
